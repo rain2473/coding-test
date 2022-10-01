46242e1cf0e973c7cf94108932c78f6350f4ebb2
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-10-01 12:04:50 +0900

    [Silver III] Title: 터렛, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int T,i;
+    int x1,y1,x2,y2;
+    int r1,r2;
+    double d=0;
+    scanf("%d",&T);
+    for(i=0;i<T;i++){
+        scanf("%d %d %d %d %d %d",&x1,&y1,&r1,&x2,&y2,&r2);
+        d = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+        if (d==0&&r1==r2){
+            printf("-1\n");
+        }
+        else if (d>r1+r2){
+            printf("0\n");
+        }
+        else if (d==r1+r2){
+            printf("1\n");
+        }
+        else if (d==abs(r1-r2)){
+            printf("1\n");
+        }
+        else if (d<abs(r1-r2)){
+            printf("0\n");
+        }
+        else {
+            printf("2\n");
+        }
+    }
+    return 0;
+}
