c4fd80a2ec5526dd7d6d1fdbf22292b377a67b1b
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-14 16:45:28 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,7 @@
 def solution(A,B):
     answer = 0
     A = sorted(A)
-    B = sorted(B)
+    B = sorted(B)[::-1]
     while len(A) != 0:
-        answer += A.pop(0)*B.pop(-1)
+        answer += A.pop(0)*B.pop(0)
     return answer
