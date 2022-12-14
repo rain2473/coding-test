80f95e7a8a7c5c4552c41ad4247b9fe03695289d
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-14 14:32:03 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,11 +1,10 @@
 def solution(n, lost, reserve):
-    _lost = sorted(list(set(lost) - set(reserve)))
-    _reserve = sorted(list(set(reserve) - set(lost)))
-    for r in _reserve:
-        f = r - 1
-        b = r + 1
-        if f in _lost:
-            _lost.remove(f)
-        elif b in _lost:
-            _lost.remove(b)
-    return n - len(_lost)
+    new_lost = sorted(list(set(lost) - set(reserve))) # 새로 선언. 덧씌우면 문제발생
+    new_reserve = sorted(list(set(reserve) - set(lost))) # 새로 선언. 덧씌우면 문제발생
+    # 2열의 lost를 덧 씌우면, 아래 3열의 reserve를 선언하는 것에 영향을 줌
+    for i in new_reserve:
+        if i - 1 in new_lost:
+            new_lost.remove(i - 1)
+        elif i + 1 in new_lost:
+            new_lost.remove(i + 1)
+    return n - len(new_lost)
