ace54861940f5e8f513d0281348457bc5d272269
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-15 18:11:23 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,9 +1,6 @@
 def solution(clothes):
-    type_clothes = []
-    for i in range(len(clothes)):
-        type_clothes.append(clothes[i][1])
-    set_type = set(type_clothes)
-    tmp = 1
-    for case in set_type:
-        tmp *= type_clothes.count(case)+1
-    return tmp - 1
+    answer = 1
+    type_clothes = [clothes[i][1] for i in range(len(clothes))]
+    for case in set(type_clothes):
+        answer *= type_clothes.count(case)+1
+    return answer - 1
