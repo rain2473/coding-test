dd807e33876343f8d307c13d9f57285aa8a1ece5
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-15 22:27:58 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -2,5 +2,4 @@ def solution(common):
     try:
         return (common[-1]-common[-2]+common[-1])*(common[-3] + common[-1] == common[-2]*2) + (common[-1]/common[-2]*common[-1])*(common[-3] * common[-1] == common[-2]**2) * (common[-1] != common[-2])
     except:
-        return (common[-1]-common[-2]+common[-1])*(common[-3] + common[-1] == common[-2]*2)
-        
+        return (common[-1]-common[-2]+common[-1])
