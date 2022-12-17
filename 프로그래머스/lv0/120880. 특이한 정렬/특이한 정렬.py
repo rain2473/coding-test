92d6a23ed29e454f6d06b92c7d3f0d61f15a5493
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-18 03:22:05 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -0,0 +1,18 @@
+def solution(numlist, n):
+    temp, plus = {}, {}
+    for i in numlist:
+        temp[i] = abs(i-n)
+        plus[i] = 2*(abs(i-n)==(i-n))-1
+    tmp = sorted(temp.values())
+    for i in range(len(tmp)):
+        for j in temp.keys():
+            if temp[j] == tmp[i]:
+                tmp[i] *= plus[j]
+                break
+        try: 
+            if abs(tmp[i]) == abs(tmp[i-1]):
+                tmp[i-1] = +abs(tmp[i])
+                tmp[i] = -abs(tmp[i])
+        except:
+            continue
+    return [i+n for i in tmp]
