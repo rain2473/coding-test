59acdd976913f005817dcc7c75b2d29ea3286484
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-17 20:50:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    for i in list(my_string):
+        try:
+            answer.append(int(i))
+        except:
+            continue
+    return sum(answer)
