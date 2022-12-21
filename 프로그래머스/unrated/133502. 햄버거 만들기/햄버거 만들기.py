653a566865e8eea5331fa3c325c7f02e43eafed0
rain2473 <71944345+rain2473@users.noreply.github.com>
2022-12-22 00:10:01 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,10 +1,10 @@
 def solution(ingredient):
     i,answer = 0,0
-    while i < len(ingredient)-3:
+    while i < len(ingredient)-1:
         if ingredient[i:i+4] == [1,2,3,1]:
             del ingredient[i:i+4]
             i -= 3
             answer += 1
-            continue
-        i += 1
+        else: 
+            i += 1
     return answer
