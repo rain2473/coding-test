921663b02dc090326b051308910ac14fae45abc0
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-01-03 18:07:06 +0900

    [level 2] Title: 행렬의 곱셈, Time: 5.11 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,4 +1,11 @@
-import numpy
 def solution(arr1, arr2):
-    answer = numpy.dot(numpy.array(arr1), numpy.array(arr2))
-    return answer.tolist()
+    answer = []
+    for arr1_row in arr1:
+        tmp = [0 for _ in range(len(arr2[0]))]
+        for i,a in enumerate(arr1_row):
+            for j in range(len(arr2[0])):
+                tmp[j] += a * arr2[i][j]
+        answer.append(tmp)
+    return answer
+            
+        
