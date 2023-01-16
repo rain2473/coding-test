50341a3ec4b950ea6f575e1019367386fc045065
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-01-17 02:36:07 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.54 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = [0 for i in range(n+1)]
+    answer[0], answer[2] = 1, 3
+    for k in range(4,n+1):
+        if k % 2 != 0:
+            continue
+        else:
+            answer[k] = answer[k-2] * 4 - answer[k-4]
+    return answer[-1] % 1000000007
