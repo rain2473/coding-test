17cdd29a42162c5aaebd623af69f72fa96602d69
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-01-17 19:36:08 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,16 +1,7 @@
-#import sys
-#sys.setrecursionlimit(10**7)
-#def Fibonacci(n):
-#    if n == 1 or n == 2:
-#        return 1
-#    else:
-#        return Fibonacci(n-2) +Fibonacci(n-1)
-#answer = Fibonacci(int(input()))
-#print(answer)
-# 재귀함수로 풀면 오류 생김
-
-n = int(input())
-answer = [0]+[1]*(n+1)
-for i in range(2,n+1):
-    answer[i] = answer[i-1] + answer[i-2]
-print(answer[n])
+def Fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return Fibonacci(n-2) +Fibonacci(n-1)
+answer = Fibonacci(int(input()))
+print(answer)
