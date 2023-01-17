32283779ae9a515f3cadc074db575fcc89321fb8
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-01-17 15:42:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,18 +1,30 @@
 import heapq
 def solution(operations):
-    answer = []
-    for order in operations:
-        try:
-            if order == "D 1":
-                answer.pop()
-            elif order == "D -1":
-                heapq.heappop(answer)
-            else:
-                tmp = int(order[2:])
-                heapq.heappush(answer,tmp)
-        except:
-            continue
-    if answer:
-        return [max(answer), min(answer)]
+    maxh, minh, visited = [], [], [False] * len(operations)
+    for i,order in enumerate(operations):
+        o, n = order.split(" ")
+        n = int(n)
+        if o == "I":
+            heapq.heappush(maxh,(-n,i))
+            heapq.heappush(minh,(n,i))
+            visited[i] = True
+        elif n == 1:
+            while maxh and visited[maxh[0][1]] == False:
+                heapq.heappop(maxh)
+            if maxh:
+                visited[maxh[0][1]] = False
+                heapq.heappop(maxh)
+        else:
+            while minh and visited[minh[0][1]] == False:
+                heapq.heappop(minh)
+            if minh:
+                visited[minh[0][1]] = False
+                heapq.heappop(minh)
+    if True in visited:
+        while maxh and visited[maxh[0][1]] == False:
+            heapq.heappop(maxh)
+        while minh and visited[minh[0][1]] == False:
+            heapq.heappop(minh)
+        return ([-maxh[0][0], minh[0][0]])
     else:
         return [0,0]
