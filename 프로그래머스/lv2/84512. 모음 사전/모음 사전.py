b94543ecc5ba68667bbd88bfa02461cfca1f2f00
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-02-10 17:03:39 +0900

    [level 2] Title: 모음 사전, Time: 5.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,8 +1,7 @@
 from itertools import product
 def solution(word):
     answer = set(map(change,product([" ","A","E","I","O","U"],repeat = 5)))
-    answer = sorted(list(answer))
-    answer = answer.index(word)
+    answer = sorted(list(answer)).index(word)
     return answer
 
 def change(tup):
