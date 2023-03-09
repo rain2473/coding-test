ade75489d1dc287883e6fc3e04b6c1d1c994954c
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-03-09 13:28:15 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean answer = (num1 == num2);
+        if (answer == true){
+            return 1;
+        } else{
+        return -1;
+        }
+    }
+}
