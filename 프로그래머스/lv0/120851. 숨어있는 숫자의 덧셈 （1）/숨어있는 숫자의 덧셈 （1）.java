43d0399435929d6e049d76cbae1b92e37e1feb8d
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-03-10 19:06:50 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.11 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for (int i = 0; i < my_string.length(); i++){
+            try{
+                answer += Integer.parseInt(String.valueOf(my_string.charAt(i)));
+            }
+            catch(NumberFormatException e){
+                    continue;
+            }
+        }
+        return answer;
+    }
+}
