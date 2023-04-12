8c91294b5948e83dbc6aae4ac0534d18cb5f4eb2
rain2473 <71944345+rain2473@users.noreply.github.com>
2023-04-12 13:04:30 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(players, callings):
+    players_hash = {}
+    rank_hash = {}
+    callings = deque(callings)
+    for i, player in enumerate(players):
+        players_hash[player] = i
+        rank_hash[i] = player
+    while callings:
+        tmp = callings.popleft()
+        chased = rank_hash[players_hash[tmp] - 1]
+        players_hash[tmp] -= 1
+        players_hash[chased] += 1
+        rank_hash[players_hash[tmp]] = tmp
+        rank_hash[players_hash[chased]] = chased
+    answer = list(rank_hash.values())
+    return answer
