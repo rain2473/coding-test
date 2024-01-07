9ce9ca8029a472ea20aaafec012c828ea22ecd72
YoonKi Hong <71944345+rain2473@users.noreply.github.com>
2024-01-07 23:12:39 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,16 +1,16 @@
 from collections import deque
 
-n = int(input())
-q = deque(enumerate(map(int, input().split())))
-ans = []
+N = int(input())
+queue = deque(enumerate(map(int, input().split(" "))))
+answer = []
 
-while q:
-    idx, paper = q.popleft()
-    ans.append(idx + 1)
-
-    if paper > 0:
-        q.rotate(-(paper - 1))
-    elif paper < 0:
-        q.rotate(-paper)
-
-print(' '.join(map(str, ans)))
+while queue:
+    idx, num = queue.popleft()
+    answer.append(str(idx + 1))
+    
+    if num > 0:
+        queue.rotate(-num + 1)
+    elif num < 0:
+        queue.rotate(-num)
+    
+print(" ".join(answer))
