e29fd26f763884bcd9e8bb41f52f76da7db86569
YoonKi Hong <71944345+rain2473@users.noreply.github.com>
2024-01-11 22:42:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 120388 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+
+heap = []
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    tmp = int(sys.stdin.readline())
+    
+    if tmp == 0:
+        try:
+            print(heapq.heappop(heap)[1])
+        except:
+            print(0)
+    else:
+        heapq.heappush(heap, [abs(tmp),tmp])
